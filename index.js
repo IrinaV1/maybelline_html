902625008bb165de4a6bcb3a5078dd2204756efc
Irina <vyustirina@gmail.com>
2024-03-10 20:28:21 +0300

    added slider js

---
+++ index.js
@@ -0,0 +1,33 @@
+const images = document.querySelectorAll('.carousel--image');
+const controls = document.querySelectorAll('.controls');
+
+let imageIndex = 0;
+
+function show(index) {
+    images[imageIndex].classList.remove('active');
+    images[index].classList.add('active');
+    imageIndex = index;
+}
+
+controls.forEach((event) => {
+    event.addEventListener('click', (event) => {
+        if (event.target.classList.contains("carousel__btn-left")) {
+            let index = imageIndex -1;
+
+            if (index < 0) {
+                index = images.length -1;
+            }
+
+            show(index);
+        } else if (event.target.classList.contains('carousel__btn-right'))
+         {
+let index = imageIndex + 1;
+if (index >= images.length) {
+    index = 0;
+}
+show(index);
+        }
+    })
+})
+
+show(imageIndex);
