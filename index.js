9a79f33c1d658759b729211078d35540008d599f
Irina <vyustirina@gmail.com>
2024-04-08 00:17:21 +0300

    added responsive footer 735px

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const images = document.querySelectorAll('.carousel--image');
 const controls = document.querySelectorAll('.controls');
-
 let imageIndex = 0;
+
 
 function show(index) {
     images[imageIndex].classList.remove('active');
     images[index].classList.add('active');
     imageIndex = index;
+  
+}
+function startSlider() {
+    intervalId = setInterval(() => {
+        let index = imageIndex + 1;
+        if (index >= images.length) {
+            index = 0;
+        }
+        show(index);
+    }, 10000); // 5 секунд
 }
 
 controls.forEach((event) => {
@@ -31,3 +41,5 @@ show(index);
 })
 
 show(imageIndex);
+
+startSlider();
